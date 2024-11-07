377bde74b05984c248f1ffb40117e353f50129c4
Dakkahi <yuyasik418@gmail.com>
2024-11-07 10:39:23 +0900

    transformerモデルの作成

--- src/Models.py
+++ src/Models.py
@@ -1,4 +1,6 @@
+import math
 import torch
+import numpy as np
 from torch import nn
 import torch.optim as optim
 import torch.nn.functional as F
@@ -44,8 +46,51 @@ class StockPriceLSTM(nn.Module):
         x = nn.functional.relu(self.fc2(x))
         return x
 
-#PsitionalEncoding用のクラスも作成
+#PositionalEncoding用のクラスも作成
+# Functions for positional encoding
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, max_len, dropout):
+        super(PositionalEncoding, self).__init__()
+        self.dropout=nn.Dropout(p=dropout)
+        pe=torch.zeros(max_len, d_model)
+        position=torch.arange(0, max_len,dtype=torch.float).unsqueeze(1)
+        div_term=torch.exp(torch.arange(0,d_model, 2).float()*(-math.log(10000.0)/d_model))
+        pe[:,0::2]=torch.sin(position*div_term)
+        pe[:,1::2]=torch.cos(position*div_term)
+        pe=pe.unsqueeze(0).transpose(0,1)
+        self.register_buffer("pe",pe)
+    
+    def forward(self,x):
+        return self.dropout(x+self.pe[:np.shape(x)[0],:])
+
+
 
 class StockPriceTransformer(nn.Module):
-    def __init__(self, input_size, d_model, nhead, num_encoder_layer, num_decoder_layer, dropout, outputsize):
+    def __init__(self, feature_size, num_layers, dropout):
         super(StockPriceTransformer, self).__init__()
+        self.model_type='Transformer'
+        self.src_mask=None
+        self.device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.pos_encoder=PositionalEncoding(d_model=feature_size)
+        self.encoder_layer=nn.TransformerEncoderLayer(d_model=feature_size,nhead=10,dropout=dropout)
+        self.transformer_encoder=nn.TransformerEncoder(self.encoder_layer,num_layers=num_layers)
+        self.decoder=nn.Linear(feature_size,1)
+    
+    def init_weights(self):
+        self.decoder.bias.data.zero_()
+        self.decoder.weight.data.uniform(-0.1,0.1)
+
+    def _generate_square_subsequent_mask(self,sz):
+        mask=(torch.triu(torch.ones(sz,sz))==1).transpose(0,1)
+        mask=mask.float().masked_fill(mask==0,float('-inf')).masked_fill(mask==1,float(0.0))
+        return mask
+
+    def forward(self,src):
+        if self.src_mask is None or self.src_mask.size(0)!=len(src):
+            device=self.device
+            mask=self._generate_square_subsequent_mask(len(src)).to(device)
+            self.src_mask=mask
+        src=self.pos_encoder(src)
+        output=self.transformer_encoder(src,self.src_mask)
+        output=self.decoder(output)
+        return output
